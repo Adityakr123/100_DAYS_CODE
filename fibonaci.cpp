371c2044308ad1dfb8920d244199c62a324eea2c
Aditya <adityakr365@gmail.com>
2024-03-26 08:52:46 +0530

    Adding logic for fibonaci series

---
+++ fibonaci.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int print_fibo(int len){
+    if(len==0){
+        return 0;
+    }
+    if (len==1){
+        return 1;
+    }
+
+}
+int main(){
+    int len=10;
+    int ans = print_fibo(len);
+    return 0;
+
+}
