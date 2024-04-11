1e1fc5885ae53e589ffef5f289a635e14f5f1d3e
Aditya Kumar <71920316+Adityakr123@users.noreply.github.com>
2024-04-12 00:59:39 +0530

    Time: 4 ms (62.95%), Space: 21.9 MB (88.29%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
         vector<int> ans;
-        sort(nums.begin(), nums.end());
-        int count = 1;
+        sort(nums.begin(),nums.end());
+        int j=1;
+        int count=1;
         for (int i = 1; i < nums.size(); i++) {
             if (nums[i] == nums[i - 1]) {
                 count++;
@@ -12,16 +13,44 @@ public:
                 count = 1;
             }
         }
-        ans.push_back(count); // Add the count for the last element
-        sort(ans.begin(), ans.end(), greater<int>()); // Sort in descending order
-        int sum = ans[0]; // Initialize sum with the frequency of the most frequent element
-        for (int k = 1; k < ans.size(); k++) {
-            if (ans[k] == ans[0]) {
-                sum += ans[k];
-            } else {
-                break; // No need to continue if we find a frequency smaller than the maximum
+        ans.push_back(count);
+        sort(ans.begin(),ans.end());
+        int temp=ans[ans.size()-1];
+        int sum=temp;
+        for(int k=0;k<(ans.size()-1);k++){
+            if(ans[k]==temp){
+                sum=sum+ans[k];
             }
         }
-        return sum;
+            
+    return sum;
     }
+    
 };
+// class Solution {
+// public:
+//     int maxFrequencyElements(vector<int>& nums) {
+//         vector<int> ans;
+//         sort(nums.begin(), nums.end());
+//         int count = 1;
+//         for (int i = 1; i < nums.size(); i++) {
+//             if (nums[i] == nums[i - 1]) {
+//                 count++;
+//             } else {
+//                 ans.push_back(count);
+//                 count = 1;
+//             }
+//         }
+//         ans.push_back(count); // Add the count for the last element
+//         sort(ans.begin(), ans.end(), greater<int>()); // Sort in descending order
+//         int sum = ans[0]; // Initialize sum with the frequency of the most frequent element
+//         for (int k = 1; k < ans.size(); k++) {
+//             if (ans[k] == ans[0]) {
+//                 sum += ans[k];
+//             } else {
+//                 break; // No need to continue if we find a frequency smaller than the maximum
+//             }
+//         }
+//         return sum;
+//     }
+// };
