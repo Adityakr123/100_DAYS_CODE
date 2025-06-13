fa8af0ae2aff82815513a7f5e0a936594fc29964
Aditya Kumar <71920316+Adityakr123@users.noreply.github.com>
2025-06-13 13:05:01 +0530

    Update char_int.cpp

--- char_int.cpp
+++ char_int.cpp
@@ -19,3 +19,14 @@ int main(){
 
 @echo File,LineCount > component_document.csv && for /R %f in (*.js *.jsx *.ts *.tsx) do @for /f "tokens=3 delims=:" %l in ('find /c /v "" "%f"') do @echo %~nxf,%l >> component_document.csv
 
+=IF(A2<=10, 0.1,
+ IF(A2<=20, 0.15,
+ IF(A2<=40, 0.26,
+ IF(A2<=60, 0.5,
+ IF(A2<=80, 0.6,
+ IF(A2<=120, 0.7,
+ IF(A2<=140, 0.8,
+ IF(A2<=150, 1,
+ IF(A2<=300, 1 + (A2-150)*(1/150),
+ IF(A2<=2658, 2 + (A2-300)*(1/2358),
+ 3))))))))))
